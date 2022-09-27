e8e3fa257978d01f55846f62702cddbe7bd46dc8
Eric Chauvin <eric.chauvin@webbfontaine.com>
2022-09-27 23:34:19 +0200

    add absences

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import json
 import os
 
 import pronotepy
-from flask import Flask
+from flask import Flask, abort
 from pronotepy import ent
 
 app = Flask(__name__)
@@ -25,7 +25,7 @@ def lessons():
 
             content = None
             if les.content is not None:
-                content = les.content.title
+                content = {'title': les.content.title, 'description': les.content.description}
 
             json = {
                 'id': les.id,
@@ -33,20 +33,41 @@ def lessons():
                 'teacher': les.teacher_name,
                 'normal': les.normal,
                 'content': content,
-                'date': les.start.date().isoformat(),
-                'start': les.start.strftime('%H:%M'),
-                'end': les.end.strftime('%H:%M'),
+                'start': les.start.isoformat(),
+                'end': les.end.isoformat(),
                 'detention': les.detention,
                 'exempted': les.exempted,
                 'status': les.status,
                 'canceled': les.canceled,
-                'room': les.classroom
+                'classroom': les.classroom
             }
             out.append(json)
 
         return out
     else:
-        return 'no login'
+        abort(500)
+
+
+@app.route('/absences')
+def absences():
+    if client.logged_in:
+        out = []
+        start = datetime.date.today()
+        end = start + datetime.timedelta(days=config['lessons']['days'])
+        for abs in client.current_period.absences:
+            json = {
+                'id': abs.id,
+                'from': abs.from_date.isoformat(),
+                'to': abs.to_date.isoformat(),
+                'hours': abs.hours,
+                'days': abs.days,
+                'reasons': abs.reasons
+            }
+            out.append(json)
+
+        return out
+    else:
+        abort(500)
 
 
 if __name__ == '__main__':
